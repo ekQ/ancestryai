5c49e83520f8af3841a092ff4aefb1c113767f5d
Marko Rasa <marko.rasa@paivola.fi>
2015-06-04 16:22:09 +0300

    vertical resizing is kinda working, but it creates a scrollbar in some cases, which could be avoided

--- main/static/js/multi-view-controller.js
+++ main/static/js/multi-view-controller.js
@@ -1,11 +1,61 @@
 
+var resizing = {
+    item: null,
+    against: null,
+    mode: "vertical",
+};
+function start_resize(item, against, mode) {
+    resizing.item = item;
+    resizing.against = against;
+    resizing.mode = mode;
+    console.warn(item.id + ", " + against.id + ", " + mode);
+    $(document).on("mousemove", resize_mousemove);
+    $(document).on("mouseup", resize_mouseup);
+}
+function resize_mousemove(event) {
+    var against_hid = "#" + resizing.against.html_id;
+    var item_hid = "#" + resizing.item.html_id;
+    if(resizing.mode == "vertical") {
+        // resize in vertical direction
+        var height1 = $(against_hid).height();
+        var height2 = $(item_hid).height();
+        var heightsum = Math.max(1, height1+height2);
+        var bary = $(item_hid).offset().top;
+        var diff = event.pageY - bary;
+        height1 += diff;
+        height2 -= diff;
+        var minimum = 50;
+        if(height1 < minimum) {
+            height2 += height1 - minimum;
+            height1 = minimum;
+        }
+        if(height2 < minimum) {
+            height1 += height2 - minimum;
+            height2 = minimum;
+        }
+        $(against_hid).height(height1);
+        $(item_hid).height(height2);
+    } else {
+        // resize in horizontal direction
+    }
+}
+function resize_mouseup(event) {
+    $(document).unbind("mousemove", resize_mousemove);
+    $(document).unbind("mouseup", resize_mouseup);
+    resizing.item = null;
+}
+
 app.controller("MultiViewController", function($scope, $translate) {
         var multi_view = this;
         this.columns = [
             ];
         var next_item_id = 0;
         var create_item = function() {
-            return {id: peek_next_id()};
+            var id = peek_next_id();
+            return {
+                id: id,
+                html_id: "ItemView"+id,
+            };
         }
         this.add_column = function() {
             var item = create_item();
@@ -18,6 +68,22 @@ app.controller("MultiViewController", function($scope, $translate) {
         };
         this.add_item = function(column_i) {
             multi_view.columns[column_i].items.push(create_item());
+        };
+        this.find_item_position = function(item) {
+            for(var i = 0; i < this.columns.length; i++)
+                for(var j = 0; j < this.columns[i].items.length; j++)
+                    if(this.columns[i].items[j].id == item.id)
+                        return [i, j];
+            return null;
+        };
+        this.resize_vertical = function(item) {
+            var pos = this.find_item_position(item);
+            if(pos === null)
+                throw Exception("trying to resize non-existent subview");
+            if(pos[1] == 0)
+                throw Exception("trying to resize top subview");
+            var against = this.columns[pos[0]].items[pos[1]-1];
+            start_resize(item, against, "vertical");
         };
         this.close_item = function(column_i, item_i) {
             // must be called from close by id
