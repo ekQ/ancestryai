c888ccbf5c3ddd27b4df256043c9957dd5a60a70
Eric Malmi <emalmi@pc97.ics.hut.fi>
2015-06-04 18:20:43 +0300

    parsing source hiski tags from gedcom

--- gedcom/hiski_kastetut.py
+++ gedcom/hiski_kastetut.py
@@ -1,7 +1,8 @@
-
+# -*- coding: utf-8 -*-
 import sys
 import re
 from gedcom import *
+import hiski_sqlalchemy as db
 
 re_hiski_kastetut = re.compile("Hiski (?P<hiskitype>[^[]+) \[(?P<hiskiid>[0-9]+)\]")
 
@@ -19,7 +20,7 @@ def CAUS(entry):
         entry.additional["hiski"][hiskitype].append(hiskiid)
 specials.CAUS = CAUS
 
-if __name__ == "__main__":
+def parse_hiski_caus():
     if len(sys.argv) < 3:
         print "python {} edgelist <gedcom-file>".format(sys.argv[0])
         print "python {} prune <gedcom-file>".format(sys.argv[0])
@@ -101,3 +102,100 @@ if __name__ == "__main__":
         reprint(root)
 
 
+re_hiski_kastetut_sour = re.compile("Hiski (?P<hiskitype>[^[]+) \[(?P<hiskiid>[0-9]+)\]")
+
+def TITL(entry):
+    if entry.value == None:
+        return
+    s = entry.value
+    for match in re_hiski_kastetut_sour.finditer(s):
+        hiskitype = match.groupdict()["hiskitype"]
+        hiskiid = match.groupdict()["hiskiid"]
+        if not "hiski" in entry.additional:
+            entry.additional["hiski"] = {}
+        if not hiskitype in entry.additional["hiski"]:
+            entry.additional["hiski"][hiskitype] = []
+        entry.additional["hiski"][hiskitype].append(hiskiid)
+specials.TITL = TITL
+
+def parse_hiski_sour():
+    if len(sys.argv) < 3:
+        print "python {} edgelist <gedcom-file>".format(sys.argv[0])
+        sys.exit()
+    root = read_file(sys.argv[2])
+    if sys.argv[1] == "edgelist":
+        individuals = {}
+        edges = []
+        sources = {}
+        # Add all individuals to the dict
+        for entry in root.traverse():
+            if entry.tag == "INDI":
+                individuals[entry.xref] = entry
+                # Family of this individual (not to be confused with
+                # entry.children and entry.parent)
+                entry.childs = []
+                entry.parents = []
+        # Parse families and sources
+        for entry in root.traverse():
+            if entry.tag == "FAM":
+                parents = []
+                children = []
+                husbxref = entry.get_chain("HUSB.value")
+                if husbxref:
+                    parents.append(individuals[husbxref])
+                wifexref = entry.get_chain("WIFE.value")
+                if wifexref:
+                    parents.append(individuals[wifexref])
+                for childxref in entry.get_multi_chain("CHIL.value"):
+                    children.append(individuals[childxref])
+
+                for parent in parents:
+                    for child in children:
+                        parent.childs.append(child)
+                        child.parents.append(parent)
+            elif entry.tag == "SOUR":
+                hiski = entry.get_chain("TITL.hiski")
+                if hiski and "kastetut" in hiski:
+                        # Take simply the first baptism ID
+                        bap_id = "-".join(hiski["kastetut"])
+                        sources[entry.xref] = bap_id
+        print "%d sources available" % len(sources)
+
+        hiski_ids = {}
+        for ind in individuals.values():
+            source_id = ind.get_chain("NAME.SOUR.value")
+            if source_id in sources:
+                hiski_id = sources[source_id]
+                firstname = ind.get_chain("NAME.GIVN.value")
+                if firstname is None:
+                    firstname = "(NN)"
+                # Get name in the Hiski DB
+                person = db.Person.query.filter(db.Person.id==hiski_id).first()
+                hiski_name = person.name.encode('utf-8')
+                hiski_ids[ind.xref] = (firstname, hiski_name, hiski_id)
+        print "%d individual hiski ids available" % len(hiski_ids)
+
+        graph = []
+        for ind in individuals.values():
+            if ind.xref not in hiski_ids:
+                continue
+            parent_id = hiski_ids[ind.xref]
+            for other in ind.childs:
+                if other.xref not in hiski_ids:
+                    continue
+                child_id = hiski_ids[other.xref]
+                if parent_id[2] != child_id[2] and parent_id[0].startswith(parent_id[1]) and child_id[0].startswith(child_id[1]):
+                    edges.append((parent_id, child_id))
+
+        for a,b in edges:
+            try:
+                a_str = "{} (H:{}) {}".format(a[0], a[1], a[2])
+                b_str = "{} (H:{}) {}".format(b[0], b[1], b[2])
+            except:
+                import extras
+                extras.keyboard()
+            print a_str + "\t" + b_str
+
+if __name__ == "__main__":
+    #parse_hiski_caus()
+    parse_hiski_sour()
---
+++ gedcom/hiski_sqlalchemy.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 17:50:54 2014
+
+@author: emalmi
+"""
+import sqlalchemy
+from sqlalchemy import func
+from elixir import *
+from scipy.stats import norm
+
+data_dir = "../../parse"
+
+a_engine = sqlalchemy.create_engine('sqlite:///%s/data_indexed.sqlite3' % data_dir, echo=False)
+a_session = sqlalchemy.orm.scoped_session(sqlalchemy.orm.sessionmaker(autoflush=True, bind=a_engine))
+a_metadata = metadata
+a_metadata.bind = a_engine
+
+b_engine = sqlalchemy.create_engine('sqlite:///%s/srkdata.sqlite3' % data_dir, echo=False)
+b_session = sqlalchemy.orm.scoped_session(sqlalchemy.orm.sessionmaker(autoflush=True, bind=b_engine))
+b_metadata = sqlalchemy.ThreadLocalMetaData()
+b_metadata.bind = b_engine
+
+class Person(Entity):
+    using_options(metadata=a_metadata, session=a_session, tablename='kastetut', autoload=True, allowcoloverride=True)
+    id = Field(Integer, colname="TAPAHTUMAID", primary_key=True)
+
+    name = Field(String, colname="LNIMI")
+    
+    # Birth date
+    year = Field(Integer, colname="SYNVU")
+    month = Field(Integer, colname="SYNKK")
+    day = Field(Integer, colname="SYNPV")
+    # Baptism date
+    byear = Field(Integer, colname="KASVU")
+    bmonth = Field(Integer, colname="KASKK")
+    bday = Field(Integer, colname="KASPV")
+    
+    village_id = Field(Integer, colname="KYLAID")
+    parish_id = Field(Integer, colname="SRKID")
+    dad_first_name = Field(String, colname="IETUN")
+    dad_last_name = Field(String, colname="ISUKUN")
+    dad_patronymic = Field(String, colname="IPATR")
+    mom_first_name = Field(String, colname="AETUN")
+    mom_last_name = Field(String, colname="ASUKUN")
+    mom_patronymic = Field(String, colname="APATR")
+    mom_age = Field(String, colname="AIKA")
+    
+    def __init__(self):
+        first_name = self.name.split('\K')[0].lower().strip()
+        if len(first_name) > 0:
+            first_name = first_name.split()[0]
+        last_name = self.dad_last_name.split('\K')[0].lower().strip()
+        self.clean_name = first_name + ' ' + last_name
+        
+    def clean_name(self, first_name, last_name):
+        first_name = first_name.split('\K')[0].lower().strip()
+        #if len(first_name) > 0:
+        #    first_name = first_name.split()[0]
+        last_name = last_name.split('\K')[0].lower().strip()
+        #patronymic = patronymic.split('\K')[0].lower().strip()
+        return first_name + ' ' + last_name
+        
+    def get_clean_name(self):
+        return self.clean_name(self.name, self.dad_last_name)
+
+    def get_clean_dad_name(self):
+        return self.clean_name(self.dad_first_name, self.dad_last_name)
+        
+    def get_clean_mom_name(self):
+        return self.clean_name(self.mom_first_name, self.mom_last_name)
+        
+    def get_label(self):
+        return self.get_clean_name() + ' ' + str(self.year)
+        
+    def get_parenthood_probability(self, child, is_father):
+        '''
+        Estimate the probability that this person (self) is the father/mother
+        of the given child (Person).
+        '''
+        age = child.year - self.year
+        p_age = -1
+        if not is_father:
+            try:
+                mom_age = int(child.mom_age)
+                if mom_age >= 14 and mom_age <= 60:
+                    p_age = norm.pdf(age,loc=mom_age,scale=2)
+            except:
+                pass
+        if p_age == -1:
+            p_age = norm.pdf(age,loc=25,scale=3)/3.0 \
+                  + norm.pdf(age,loc=35,scale=5)/3.0 \
+                  + norm.pdf(age,loc=30,scale=5)/3.0
+                  
+        if self.village_id == child.village_id:
+            p_village = 0.7
+        else:
+            p_village = 0.3
+        return p_age * p_village
+    
+class Village(Entity):
+    using_options(metadata=b_metadata, session=b_session, tablename='kylat', autoload=True, allowcoloverride=True)
+    id = Field(Integer, colname="id", primary_key=True)
+    parish_id = Field(Integer, colname="seurakunta")
+    name = Field(String, colname="kyla")
+
+class Parish(Entity):
+    using_options(metadata=b_metadata, session=b_session, tablename='seurakunnat', autoload=True, allowcoloverride=True)
+    id = Field(Integer, colname="id", primary_key=True)
+    name = Field(String, colname="namefi")
+    lon = Field(Float, colname="lon")
+    lat = Field(Float, colname="lat")
+    
+setup_all()
